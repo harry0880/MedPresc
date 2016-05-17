5db01b110dd277e9c5e70c48c2c75a2c88c8e1ac
harry0880 <harpreet0880@gmail.com>
2016-05-17 17:15:44 +0530

    hanshal test

---
+++ app/src/main/java/com/medpresc/ActivityViewPatient.java
@@ -0,0 +1,76 @@
+package com.medpresc;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.Toast;
+
+import com.dexafree.materialList.card.Card;
+import com.dexafree.materialList.card.CardProvider;
+import com.dexafree.materialList.card.OnActionClickListener;
+import com.dexafree.materialList.card.action.TextViewAction;
+import com.dexafree.materialList.card.action.WelcomeButtonAction;
+import com.dexafree.materialList.view.MaterialListView;
+import com.squareup.picasso.RequestCreator;
+
+public class ActivityViewPatient extends AppCompatActivity {
+
+    private MaterialListView mListView;
+    int cnt;
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_view_patient);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mContext = this;
+
+        // Bind the MaterialListView to a variable
+        mListView = (MaterialListView) findViewById(R.id.material_listview);
+        //  mListView.setItemAnimator(new SlideInLeftAnimator());
+        mListView.getItemAnimator().setAddDuration(300);
+        mListView.getItemAnimator().setRemoveDuration(300);
+       while(cnt<5) {
+           mListView.getAdapter().add(generate());
+            cnt++;
+       }
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Hanshal", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private Card generate()
+    {
+        return  new Card.Builder(this)
+                .setTag("Medicine")
+                .withProvider(new CardProvider())
+                .setLayout(R.layout.material_welcome_card_layout)
+                .setTitle("Medicine Name")
+                //.setTitleColor(Color.WHITE)
+                .setDescription("After Meals")
+              //  .setDescriptionColor(Color.WHITE)
+                .setSubtitle("Twice a Day")
+                .setSubtitleColor(Color.BLACK)
+               // .setBackgroundColor(Color.BLUE)
+                .endConfig().build();
+    }
+
+}
