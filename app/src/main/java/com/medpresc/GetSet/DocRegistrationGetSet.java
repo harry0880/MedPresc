8ffd9dc0c4fefd7713d6a9ec8dc535b1f3aa15d9
harry0880 <harpreet0880@gmail.com>
2016-05-18 11:59:04 +0530

    Merge branch 'master' of https://github.com/harry0880/MedPresc
    Conflicts:
    	app/src/main/java/com/medpresc/ActivityViewPatient.java
    	app/src/main/java/com/medpresc/DbConstant.java

---
+++ app/src/main/java/com/medpresc/GetSet/DocRegistrationGetSet.java
@@ -0,0 +1,84 @@
+package com.medpresc.GetSet;
+
+/**
+ * Created by Hanshal on 5/18/2016.
+ */
+public class DocRegistrationGetSet {
+    String Name,EmailID,PhoneNumber,State,District,Institute,Speciality,RegType,RegNumber;
+    public String getName() {
+        return Name;
+    }
+
+    public String getEmailID() {
+        return EmailID;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public String getDistrict() {
+        return District;
+    }
+
+    public String getInstitute() {
+        return Institute;
+    }
+
+    public String getSpeciality() {
+        return Speciality;
+    }
+
+    public String getRegType() {
+        return RegType;
+    }
+
+    public String getRegNumber() {
+        return RegNumber;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setEmailID(String emailID) {
+        EmailID = emailID;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public void setDistrict(String district) {
+        District = district;
+    }
+
+    public void setInstitute(String institute) {
+        Institute = institute;
+    }
+
+    public void setSpeciality(String speciality) {
+        Speciality = speciality;
+    }
+
+    public void setRegType(String regType) {
+        RegType = regType;
+    }
+
+    public void setRegNumber(String regNumber) {
+        RegNumber = regNumber;
+    }
+
+
+
+
+}
+
